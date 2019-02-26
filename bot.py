91637ae90c4952b02a1dab13276843877b3e2148
sarna <sarna.dev@protonmail.com>
2019-02-26 14:04:27 +0100

    Add greetings and goodbyes

--- bot.py
+++ bot.py
@@ -1,31 +1,47 @@
 import os
-import asyncio
+from discord.ext import commands
+prefix = '!'
+bot = commands.Bot(command_prefix=prefix)
 
-import discord
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged in as')
-        print(self.user.name)
-        print(self.user.id)
-        print('------')
+@bot.event
+async def on_ready():
+    print("I'm in your area")
 
-    async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
-        if message.content.startswith('!test'):
-            counter = 0
-            tmp = await message.channel.send('Calculating messages...')
-            async for msg in message.channel.history(limit=100):
-                if msg.author == message.author:
-                    counter += 1
 
-            await tmp.edit(content='You have {} messages.'.format(counter))
-        elif message.content.startswith('!sleep'):
-            with message.channel.typing():
-                await asyncio.sleep(5.0)
-                await message.channel.send('Done sleeping.')
+@bot.listen()
+async def on_member_join(member):
+    channel = member.guild.system_channel
+    msg = (
+        ":postal_horn: "
+        f"Greetings, {member.mention}. I am Heimdallr, he who stands "
+        "stands over this Server. State your level so that I may grant you "
+        "your right tag. Fear not if you have questions, our moderators "
+        "and mentors shall answer them."
+    )
+    msg_dk = (
+        ":postal_horn: "
+        f"Vær hilset, {member.mention}. Jeg er Heimdallr, han som vogter "
+        "over denne Server. Fortæl mig dit niveau så jeg kan give dig din "
+        "rette tag. Frygt ej hvis du haver spørgsmål, for vore moderatorer "
+        "og mentorer skal nok svare på dem."
+    )
+    await channel.send(f"{msg}\n\n{msg_dk}")
 
-client = MyClient()
-client.run(os.environ.get('TOKEN'))
+
+@bot.listen()
+async def on_member_remove(member):
+    channel = member.guild.system_channel
+    msg = (f":rainbow: Farewell, {member.display_name}.")
+    await channel.send(f"{msg}")
+
+
+@bot.command()
+async def ping(ctx):
+    '''
+    Check if bot is online
+    '''
+    latency = bot.latency
+    await ctx.send(f"Latency: {latency}")
+
+bot.run(os.environ.get('TOKEN'))
